9dd7e1368406c1a908e59337b57f3dba256360e3
S_GADIOUX <sebgad19@gmail.com>
2018-03-06 11:17:54 +0100

    Fichier de test

--- inventrry/inst/python/Test/test.py
+++ inventrry/inst/python/Test/test.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 import sys
 sys.path.append('../')
-import imbalance
+import imbalance as ib
 import util as ut
 
 invTest = pd.read_csv("aarInv.csv")
 specTest = pd.read_csv("aarSpecs.csv")
 
-print(ut.binary_counts( invTest['high'] ))
-k = ut.otn2( invTest['high'] )
-print(k	)
+who = invTest.loc[invTest['segment_type']=='Whole']
+spe0 = specTest.iloc[0]
 
-print(ut.binary_counts( k))
+nimb = ib.nimbalance(who, spe0)
+print(nimb)
