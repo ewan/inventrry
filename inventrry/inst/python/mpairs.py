13f0fc9cc273daabe36caccaba3ad7b660aff65a
S_GADIOUX <sebgad19@gmail.com>
2018-03-06 12:30:07 +0100

    work in progresse on nmpairs

--- inventrry/inst/python/mpairs.py
+++ inventrry/inst/python/mpairs.py
@@ -10,3 +10,12 @@ def mpairs(inventory, spec, feature):
     partition = to_row_partition(get_cols_except(inventory, spec, feature))
     n_equivalence_classes = len(partition)
     return inventory.shape[0] - n_equivalence_classes
+
+def nmpairs(inventory, spec):
+	trueSpec = [f for f in spec.iloc[:][4:27].keys() if spec.iloc[:][f]]
+	
+	print('####trueSpec : ####')
+	print(trueSpec)
+	
+	mpairs(inventory, spec[4:27], trueSpec[0])
+	#return sum(mpairs(inventory, spec, feat) for feat in trueSpec)
--- inventrry/inst/python/util.py
+++ inventrry/inst/python/util.py
@@ -20,7 +20,15 @@ def otn(operators):
     return k
 
 def get_cols_except(matrix, cols, no_go_col):
+    print(cols)
+    print(len(cols))
     cols_to_get = tuple([c for c in cols if c != no_go_col])
+    print("####cols_to_get : ####")
+    print(cols_to_get)
+    print(len(cols_to_get))
+    print("####Test Matrix: ####")
+    print(matrix[:,cols_to_get])
+    print("####End Test####")
     return matrix[:,cols_to_get]
 
 def binary_counts(vec):
