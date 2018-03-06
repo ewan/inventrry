0d69b48746e35d13b45e1fc73a68c0bdeed6a9da
S_GADIOUX <sebgad19@gmail.com>
2018-03-06 12:30:22 +0100

    work in progresse on nmpairs

--- inventrry/inst/python/Test/test.py
+++ inventrry/inst/python/Test/test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 sys.path.append('../')
 import imbalance as ib
+import mpairs as mp
 import util as ut
 
 invTest = pd.read_csv("aarInv.csv")
@@ -10,6 +11,5 @@ specTest = pd.read_csv("aarSpecs.csv")
 
 who = invTest.loc[invTest['segment_type']=='Whole']
 spe0 = specTest.iloc[0]
-
-nimb = ib.nimbalance(who, spe0)
-print(nimb)
+nmp = mp.nmpairs(who, spe0)
+print(nmp)
