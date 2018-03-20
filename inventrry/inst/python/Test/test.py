cee04887facf3cf6df55471d73a6a2fb0afafe12
S_GADIOUX <sebgad19@gmail.com>
2018-03-20 12:19:55 +0100

    Working on a script function

--- inventrry/inst/python/Test/test.py
+++ inventrry/inst/python/Test/test.py
@@ -11,6 +11,26 @@ invTest = pd.read_csv("aarInv.csv")
 specTest = pd.read_csv("aarSpecs.csv")
 normTab = pd.read_feather("geoms.feather")
 
+sType = ['Whole', 'Consonant','Stop/affricate', 'Vowel']
+
+def script( sType, invTest, specTest, normTab ) : 
+	mainDict = {}
+	for t in sType :
+		mainDict[t] = {'e' : [] , 'l' : [], 'g' : []}
+		inv = invTest.loc[invTest['segment_type']==t]
+		spe = specTest.loc[specTest['segment_type']==t]
+		l = spe.shape[0] 
+		for spec in range(l) : 
+			e, l, g = st.stats(inv, (ut.getTrueSpecs(spe.iloc[spec].to_dict())),normTab)
+			mainDict[t]['e'].append(e)
+			mainDict[t]['l'].append(l)
+			mainDict[t]['g'].append(g)
+		for i in mainDict[t] :
+			mainDict[t][i] = np.median(mainDict[t][i])
+	return mainDict
+
+test = script(sType, invTest, specTest, normTab)
+'''
 who = invTest.loc[invTest['segment_type']=='Stop/affricate']
 spe = []
 for i in range(145,150) :
@@ -24,3 +44,4 @@ for spec in spe :
 
 for i in test :
 	print(np.median(test[i]))
+'''
