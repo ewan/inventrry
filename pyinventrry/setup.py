2cc1b9a0e095f49b04e672367d9c101098033550
S-GADIOUX <sebgad19@gmail.com>
2018-04-17 11:47:08 +0200

    Upgranding the setp for better cleaning

--- pyinventrry/setup.py
+++ pyinventrry/setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+import os
+from setuptools import setup, Command
 
-setup(name='pyinventrry',
-		version='1',
-		description='In progress',
-		url='http://github.com/ewan/inventry',
-		author='Ewan DUNBAR & Sebastien GADIOUX',
-		author_email='',
-		license='',
-		packages=['pyinventrry','pyinventrry.score'],
-		zip_safe=False)
+class CleanCommand(Command):
+	"""Custom clean command to tidy up the project root."""
+	user_options = []
+	def initialize_options(self):
+		pass
+	def finalize_options(self):
+		pass
+	def run(self):
+		os.system('rm -vrf ./build ./dist ./*.pyc  ./*.egg-info')
+
+# Further down when you call setup()
+setup(
+	cmdclass={ 'clean': CleanCommand},
+	name='pyinventrry',
+	version='1',
+	description='In progress',
+	url='http://github.com/ewan/inventry',
+	author='Ewan DUNBAR & Sebastien GADIOUX',
+	author_email='',
+	license='',
+	packages=['pyinventrry','pyinventrry.score'],
+	zip_safe=False
+)
