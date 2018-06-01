7c315840b2664ea6e9adb8a4cbde160affec09e5
S-GADIOUX <sebgad19@gmail.com>
2018-06-01 15:28:54 +0200

    Specs finally working

--- pyinventrry/pyinventrry/spec.py
+++ pyinventrry/pyinventrry/spec.py
@@ -3,8 +3,7 @@ import sys as  _sys
 import argparse
 import pyinventrry.data_manager as _dm
 
-def split_by(feat, part, global_part):
-
+def splitter(feat, part):
 	plus = []
 	minus = []
 	none = []
@@ -15,6 +14,12 @@ def split_by(feat, part, global_part):
 			minus.append(phon)
 		else :
 			none.append(phon)
+
+	return plus, minus, none	
+
+def split_by(feat, part, global_part):
+
+	plus, minus, none = splitter(feat, part)
 				
 	if ((len(part) == len(plus)) or (len(part) == len(minus)) or (len(part) == len(none))):
 		global_part.append(part)
@@ -31,26 +36,14 @@ def split_by(feat, part, global_part):
 
 def compare(s, t):
 	# Is s better than t 
-	b = False
-	sPart = []
-	for p in s :
-		sPart.append(len(p))
-	print(sPart)
-	tPart = []
-	for p in t :
-		tPart.append(len(p))
-	print(tPart)
-	print('--------')
-	t = list(t) 
+	
+	t = list(t)   # make a mutable copy
 	try:
 		for elem in s:
 			t.remove(elem)
 	except (ValueError, TypeError) as e:
-		b =  False
-	if not b :
 		return False
-	b = not t
-	return b
+	return not t
 
 class Node:
 
@@ -116,35 +109,10 @@ class Node:
 					self.next[n].get_specs( acc, list(actual))
 			return 
 
-def tree_theory(phones, feat_set):
+def tree_theory(feat_set, phones):
 	top = Node(p = None, s = None, o = set(feat_set), pa = phones, n = {})
 	top.generate()
 	return top.get_specs(acc = [], actual = [])
-
-def data_frame_theory(feat_set, phones, deep):
-	specs = []
-	for f in feat_set :
-		done = True
-		tmp = set(feat_set)
-		tmp.remove(f)
-		rmv = phones.loc[:,tmp].drop_duplicates()
-		if rmv.shape[0] == 2:
-
-			specs.append([f])
-
-		if rmv.shape[0] != phones.shape[0]:
-			done = False
-			part = data_frame_theory(tmp, rmv, deep +1 )
-			for s in part:
-				s.append(f)
-				specs.append(s)
-		if (pow(2, deep)>= rmv.shape[0]):
-			specs.append([f])
-	return specs
-
-
-
-	input('Be ready for ^C')
 
 def extract_from_file(file_name):
 	df = _pd.read_csv(file_name)
@@ -172,15 +140,34 @@ def extract_phones(data_frame, feat_dict):
 		phones.append(phon)
 	return phones
 	
+def check_spec(spec, phones):
+	b = True
+	for feat in spec:
+		s = set(spec)
+		s.remove(feat)
+		b = b and phones.loc[:,s].drop_duplicates().shape[0] != phones.loc[:,spec].drop_duplicates().shape[0]
+
+	return b
+
 def calculate_all_specs(file_name):
 	inventories, meta_keys, unique_list, feat_list = extract_from_file(file_name)
 	feat_dict = calculate_feat_dict(feat_list)
 	feat_set = set(feat_dict)
 	df = _pd.DataFrame(columns=meta_keys + ['_spec_nb'] + feat_list)
 	for unique in unique_list :
-		phones = extract_phones(_dm.extract_data_frame(inventories, unique), feat_dict )
-		phones = _dm.extract_data_frame(inventories, unique)
-		specs = data_frame_theory(feat_list, phones, 0)
+		df_phones = phones = _dm.extract_data_frame(inventories, unique)
+		
+		phones = extract_phones(df_phones, feat_dict )
+		int_specs = tree_theory(feat_set, [phones])
+		
+		specs = []
+		for int_spec in int_specs :
+			spec = set()
+			for i in int_spec:
+				spec.add(feat_dict[i])
+			if check_spec(spec, df_phones):
+				specs.append(spec)
+
 		i = 1
 		for spec in specs :
 			d = dict(unique)
@@ -191,44 +178,17 @@ def calculate_all_specs(file_name):
 			for t in spec:
 				d[t] = 'True'
 			df = df.append(d, ignore_index=True)
-	
 	return df
-
-def ready_datas(file_name) :
-	df = _pd.read_csv(file_name)
-	
-	features = df.columns.values.tolist()
-	feat_dict = calculate_feat_dict(features)
-
-	phones = extract_phones(df, feat_dict)
-
-	return feat_dict,phones,len(feat_dict)
-
-def turn_spec_number(feat_dict, feat_tuple):
-	returN = []
-	for i in feat_tuple:
-		returN.append(feat_dict[i])
-	return returN
-
-def get_from_small(file_name):
-	feat_dict,phones,i = ready_datas(file_name)
-	specs = calculate_spec(phones,feat_dict)
-	for u in specs :
-		print(turn_spec_number(feat_dict, u))
 
 def main() :
 	parser = argparse.ArgumentParser()
 	parser.add_argument("inventory", help = 'A file with the correct format for the inventory')
 	parser.add_argument("-w", "--write", default=_sys.stdout,)
-	parser.add_argument("-s", "--small", action="store_true")
 	args = parser.parse_args(_sys.argv[1:])
 	file_name = args.inventory
 	write_file = args.write
-	if args.small :
-		get_from_small(file_name)
-	else :
-		df = calculate_all_specs(file_name)
-		df.to_csv(write_file, mode = 'w', header=True, index=False)
+	df = calculate_all_specs(file_name)
+	df.to_csv(write_file, mode = 'w', header=True, index=False)
 
 if __name__ == "__main__":
 	main()
