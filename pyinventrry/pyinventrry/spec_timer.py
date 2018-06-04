b943b557c7c158f4d41493896095452b7fa5eb73
S-GADIOUX <sebgad19@gmail.com>
2018-06-04 16:48:13 +0200

    Timing spec with comparaison theory results

--- pyinventrry/pyinventrry/spec_timer.py
+++ pyinventrry/pyinventrry/spec_timer.py
@@ -6,6 +6,16 @@ import sys as  _sys
 import argparse
 import pandas as _pd
 
+_ti.template = """
+def inner(_it, _timer{init}):
+    {setup}
+    _t0 = _timer()
+    for _i in _it:
+        retval = {stmt}
+    _t1 = _timer()
+    return _t1 - _t0, retval
+"""
+
 def wrapper(func, *args, **kwargs):
 	def wrapped():
 		return func(*args, **kwargs)
@@ -23,45 +33,67 @@ def calculate_one (feat_set, phones, feat_dict, df_phones):
 			specs.append(spec)
 	return specs
 
-
 def time_specs(file_name) :	
 	inventories, meta_keys, unique_list, feat_list = _sp.extract_from_file(file_name)
 	feat_dict = _sp.calculate_feat_dict(feat_list)
 	feat_set = set(feat_dict)
 	time_dict = {}
+	df = _pd.DataFrame(columns=meta_keys + ['_spec_nb'] + feat_list)
+
 	for unique in unique_list :
 		df_phones = _dm.extract_data_frame(inventories, unique)
 		phones = _sp.extract_phones(df_phones, feat_dict )
 		
 		wrapped = wrapper(calculate_one,feat_set,phones,feat_dict,df_phones)
 		size = len(phones)
-		time = _ti.timeit(wrapped,setup ="gc.enable"  ,number =1)
+		
+		time, specs = _ti.timeit(wrapped,setup ="gc.enable"  ,number =1)
+		
 		if size in time_dict :
 			time_dict[size].append(time)
 		else :
 			time_dict[size] = [time]
-	df = _pd.DataFrame(columns=['Size','Median','Variance'])
+
+		i = 1
+		for spec in specs :
+			d = dict(unique)
+			d['_spec_nb'] = 'Spec'+str(i)
+			i+=1
+			for feat in feat_list:
+				d[feat] = 'False'
+			for t in spec:
+				d[t] = 'True'
+			df = df.append(d, ignore_index=True)
+
+
+	time_frame = _pd.DataFrame(columns=['Size','Median','Variance'])
 	
 	for s in time_dict:
-		print(s)
 		d = {'Size' : s,'Median' :_np.median(time_dict[s]),'Variance' : _np.var(time_dict[s])}
-		print(d)
-		df = df.append(d, ignore_index=True)
+		time_frame = time_frame.append(d, ignore_index=True)
 
-	return df.sort_values('Size')
+	return time_frame.sort_values('Size'),df
 
-
+def compare(theory, real):
+	t = (theory.drop('_spec_id', axis = 1).to_dict('records'))
+	r = (real.drop('_spec_nb', axis = 1).to_dict('records'))
+	print(real)
+	d_t = [x for x in r if x not in t]
+	print(d_t)
 
 def main() :
 	parser = argparse.ArgumentParser()
 	parser.add_argument("inventory", help = 'A file with the correct format for the inventory')
+	parser.add_argument("theory", help = 'A file with the correct format for the inventory')
 	parser.add_argument("-w", "--write", default=_sys.stdout)
 	args = parser.parse_args(_sys.argv[1:])
 	file_name = args.inventory
 	write_file = args.write
-	df = time_specs(file_name)
+	df, spec_frame = time_specs(file_name)
 	df.to_csv(write_file, mode = 'w', header=True, index=False)
-	print(df)
+	theory_frame = _dm.open_file(args.theory)
+	compare(theory_frame, spec_frame)
+	
 		
 if __name__ == "__main__":
 	main()
